691e80680c94c24dcfbcf847d05b9c101dbd603f
jbytecode <mhsatman@gmail.com>
2022-08-25 21:42:40 +0300

    format code

--- statsbase.v
+++ statsbase.v
@@ -1,135 +1,134 @@
-module main 
+module main
 
-import math 
+import math
 
 fn geomean(x []f64) f64 {
-    mut s := 0.0
-    xl := x.len
-    denom := 1.0 / f64(xl)
-    for i in 0 .. xl {
-        s += math.log(x[i])
-    }
-    return math.exp(denom * s)
+	mut s := 0.0
+	xl := x.len
+	denom := 1.0 / f64(xl)
+	for i in 0 .. xl {
+		s += math.log(x[i])
+	}
+	return math.exp(denom * s)
 }
 
 fn mad(x []f64) f64 {
-    med := median(x)
-    xl := x.len
-    mut res := []f64{len: xl} 
-    for i in 0 .. xl {
-        res[i] = math.abs(x[i] - med)
-    }
-    return median(res) * 1.4826022185056018
+	med := median(x)
+	xl := x.len
+	mut res := []f64{len: xl}
+	for i in 0 .. xl {
+		res[i] = math.abs(x[i] - med)
+	}
+	return median(res) * 1.4826022185056018
 }
 
 /*
-    Pearson Correlation Coefficient
+Pearson Correlation Coefficient
 */
 fn correlation(x []f64, y []f64) f64 {
-    xstd := standardize(x)
-    ystd := standardize(y)
-    xl := x.len
-    mut s := 0.0
-    for i in 0..xl {
-        s += xstd[i] * ystd[i]
-    }
-    return s / (f64(xl) - 1.0)
+	xstd := standardize(x)
+	ystd := standardize(y)
+	xl := x.len
+	mut s := 0.0
+	for i in 0 .. xl {
+		s += xstd[i] * ystd[i]
+	}
+	return s / (f64(xl) - 1.0)
 }
 
 fn covariance(x []f64, y []f64) f64 {
-    centeredx := centerize(x)
-    centeredy := centerize(y)
-    mut s := 0.0
-    for i in 0..x.len {
-        s += centeredx[i] * centeredy[i]
-    }
-    return s / (f64(x.len) - 1.0)
+	centeredx := centerize(x)
+	centeredy := centerize(y)
+	mut s := 0.0
+	for i in 0 .. x.len {
+		s += centeredx[i] * centeredy[i]
+	}
+	return s / (f64(x.len) - 1.0)
 }
 
 fn sample_std(x []f64) f64 {
-    return math.sqrt(sample_variance(x))
+	return math.sqrt(sample_variance(x))
 }
 
 fn sample_variance(x []f64) f64 {
-    centered := centerize(x)
-    mut s := f64(0.0)
-    for element in centered {
-        s += element * element 
-    }
-    return s / (f64(x.len) - 1.0)
+	centered := centerize(x)
+	mut s := f64(0.0)
+	for element in centered {
+		s += element * element
+	}
+	return s / (f64(x.len) - 1.0)
 }
 
 fn standardize(x []f64) []f64 {
-    mut centered := centerize(x)
-    stddev := sample_std(x)
-    for i in 0..x.len {
-        centered[i] /= stddev 
-    }
-    return centered 
+	mut centered := centerize(x)
+	stddev := sample_std(x)
+	for i in 0 .. x.len {
+		centered[i] /= stddev
+	}
+	return centered
 }
 
 fn centerize(x []f64) []f64 {
-    xl := x.len
-    mut res := []f64{len: x.len}
-    avg := mean(x)
-    for i in 0..xl {
-        res[i] = x[i] - avg 
-    }
-    return res 
+	xl := x.len
+	mut res := []f64{len: x.len}
+	avg := mean(x)
+	for i in 0 .. xl {
+		res[i] = x[i] - avg
+	}
+	return res
 }
 
 fn max(x []f64) f64 {
-    mut res := math.inf(-1)
-    for element in x {
-        if element > res {
-            res = element 
-        }
-    } 
-    return res 
+	mut res := math.inf(-1)
+	for element in x {
+		if element > res {
+			res = element
+		}
+	}
+	return res
 }
 
-
 fn min(x []f64) f64 {
-    mut res := math.inf(1)
-    for element in x {
-        if element < res {
-            res = element 
-        }
-    } 
-    return res 
+	mut res := math.inf(1)
+	for element in x {
+		if element < res {
+			res = element
+		}
+	}
+	return res
 }
 
 fn median(x []f64) f64 {
-    return quantile(x, 0.5)
+	return quantile(x, 0.5)
 }
 
 fn quantile(x []f64, p f64) f64 {
-    n := x.len 
-    h := (n - 1) * p 
-    hdown := math.floor(h)
-    hup   := math.ceil(h)
-    mut sortedx := x.clone()
-    sortedx.sort()
-    xup := sortedx[u64(hup)]
-    xdown := sortedx[u64(hdown)]
-    return xdown + ((h - hdown) * (xup - xdown))
+	n := x.len
+	h := (n - 1) * p
+	hdown := math.floor(h)
+	hup := math.ceil(h)
+	mut sortedx := x.clone()
+	sortedx.sort()
+	xup := sortedx[u64(hup)]
+	xdown := sortedx[u64(hdown)]
+	return xdown + ((h - hdown) * (xup - xdown))
 }
 
 fn sum(x []f64) f64 {
-    mut s := f64(0.0)
-    for i in 0 .. x.len {
-        s += x[i]
-    }
-    return s
+	mut s := f64(0.0)
+	for i in 0 .. x.len {
+		s += x[i]
+	}
+	return s
 }
 
 fn mean(x []f64) f64 {
-    if x.len == 0 {
-        return 0.0
-    }
-    return sum(x) / x.len
+	if x.len == 0 {
+		return 0.0
+	}
+	return sum(x) / x.len
 }
 
 fn main() {
-    print("Hello world!")
+	print('Hello world!')
 }
--- statsbase_test.v
+++ statsbase_test.v
@@ -1,12 +1,12 @@
-module main 
+module main
 
-import math 
+import math
 
 fn test_sum() {
 	assert 0.0 == sum([])
 	assert 1.0 == sum([1.0])
 	assert 2.0 == sum([1.0, 1])
-	assert 15.0 == sum([1.0,2,3,4,5])
+	assert 15.0 == sum([1.0, 2, 3, 4, 5])
 }
 
 fn test_mean() {
@@ -17,10 +17,10 @@ fn test_mean() {
 }
 
 fn test_quantile() {
-	x := [1.0 2 3 4 5 6 7 8 9 10]
-	assert 1.0  == quantile(x, 0.0) // Min 
+	x := [1.0, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	assert 1.0 == quantile(x, 0.0) // Min
 	assert 10.0 == quantile(x, 1.0) // Max
-	assert 5.5  == quantile(x, 0.5) // Median
+	assert 5.5 == quantile(x, 0.5) // Median
 }
 
 fn test_median() {
@@ -44,57 +44,56 @@ fn test_max() {
 	assert 10.0 == max(y)
 }
 
-fn test_centerize(){
+fn test_centerize() {
 	x := [1.0, 2.0, 3.0]
 	y := centerize(x)
-	assert -1  == y[0]
+	assert -1 == y[0]
 	assert 0.0 == y[1]
-	assert  1  == y[2]
+	assert 1 == y[2]
 }
 
-fn test_standardize(){
-	x := [1.0, 2.0, 3.0, 10.0, 9.0] 
+fn test_standardize() {
+	x := [1.0, 2.0, 3.0, 10.0, 9.0]
 	stdx := standardize(x)
-	assert -0.9561828874675149  == stdx[0]
- 	assert -0.7171371656006361  == stdx[1]
- 	assert -0.47809144373375745 == stdx[2]
-  	assert 1.1952286093343936   == stdx[3]
-  	assert 0.9561828874675149   == stdx[4]
+	assert -0.9561828874675149 == stdx[0]
+	assert -0.7171371656006361 == stdx[1]
+	assert -0.47809144373375745 == stdx[2]
+	assert 1.1952286093343936 == stdx[3]
+	assert 0.9561828874675149 == stdx[4]
 }
 
-fn test_sample_variance(){
+fn test_sample_variance() {
 	x := [1.0, 2.0, 3.0, 10.0, 9.0]
 	assert 17.5 == sample_variance(x)
 }
 
-fn test_sample_std(){
+fn test_sample_std() {
 	x := [1.0, 2.0, 3.0, 10.0, 9.0]
 	assert 4.183300132670378 == sample_std(x)
 }
 
-fn test_covariance(){
+fn test_covariance() {
 	x := [1.0, 2.0, 3.0, 10.0, 9.0]
 	y := [9.0, 10, 3, 2, 1]
 	assert 17.5 == covariance(x, x)
 	assert -14.5 == covariance(x, y)
 }
 
-fn test_correlation(){
+fn test_correlation() {
 	eps := 0.0001
 	x := [1.0, 2.0, 3.0, 10.0, 9.0]
 	y := [9.0, 10, 3, 2, 1]
-	assert math.abs(1.0 - correlation(x, x)) < eps 
-	assert math.abs(-0.8285714285714286 - correlation(x, y)) < eps 
+	assert math.abs(1.0 - correlation(x, x)) < eps
+	assert math.abs(-0.8285714285714286 - correlation(x, y)) < eps
 }
 
-fn test_mad(){
+fn test_mad() {
 	x := [1.0, 2.0, 3.0, 10.0, 9.0]
 	xmad := mad(x)
-	assert 2.9652044370112036 == xmad 
+	assert 2.9652044370112036 == xmad
 }
 
-fn test_geomean(){
+fn test_geomean() {
 	x := [1.0, 2.0, 3.0, 10.0, 9.0]
 	assert 3.5194820289355238 == geomean(x)
 }
-
